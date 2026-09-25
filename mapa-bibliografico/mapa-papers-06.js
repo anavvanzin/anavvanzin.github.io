bf4ae523d6b0d2f14cbf324174d3364d6bf07997
anavvanzin <ana@anavanzin.com>
2026-09-25 02:58:15 -0300

    feat(mapa): papers, parte 6/9

---
+++ mapa-bibliografico/mapa-papers-06.js
@@ -0,0 +1,363 @@
+window.MAPA_DATA.papers.push(...[
+{
+ "title": "Hércules, Hermes ea Pequena Sereia: Uma reflexão sobre estereótipos de gênero, subrepresentação das mulheres nos tribunais ea (i) legitimidade …",
+ "authors": [
+  "JRG Pereira",
+  "RM de Oliveira"
+ ],
+ "year": 2018,
+ "venue": "JRG Pereira, RM de Oliveira - Revista Brasileira de Políticas …, 2018 - academia.edu",
+ "citation_count": 21,
+ "abstract": "… nos tribunais superiores e na justiça federal, que demonstram … federal, para expor a baixa representatividade feminina na … é estabelecido por meio de alegorias masculinas. Mas, por …",
+ "url": "https://www.academia.edu/download/57458486/Hercules_Hermes_e_a_Pequena_Sereira_Uma_-_copia.pdf",
+ "pdf_links": [],
+ "themes": [
+  "alegorias femininas da justiça"
+ ],
+ "source_query": "alegorias femininas da justiça"
+},
+{
+ "title": "Uma justiça feminina? A Brasilidade restaurativa construída por mulheres",
+ "authors": [
+  "JP Macedo",
+  "MJP Advincula",
+  "PM Graf…"
+ ],
+ "year": null,
+ "venue": "JP Macedo, MJP Advincula, PM Graf… - … NA PESQUISA: Reflexão … - researchgate.net",
+ "citation_count": 2,
+ "abstract": "… que a concepção de justiça é feminina. Isso porque, no mundo ocidental moderno, a Deusa Têmis é a alegoria mais forte da representatividade imagética e conceitual de justiça: uma …",
+ "url": "https://www.researchgate.net/profile/Larissa-Wachholz/publication/356954052_Mulheres_na_pesquisa_Reflexao_sobre_as_mulheres_Em_seus_diversos_espacos_de_atuacao/links/62602ee1bca601538b5a2e94/Mulheres-na-pesquisa-Reflexao-sobre-as-mulheres-Em-seus-diversos-espacos-de-atuacao.pdf#page=93",
+ "pdf_links": [
+  "https://www.researchgate.net/profile/Larissa-Wachholz/publication/356954052_Mulheres_na_pesquisa_Reflexao_sobre_as_mulheres_Em_seus_diversos_espacos_de_atuacao/links/62602ee1bca601538b5a2e94/Mulheres-na-pesquisa-Reflexao-sobre-as-mulheres-Em-seus-diversos-espacos-de-atuacao.pdf#page=93"
+ ],
+ "themes": [
+  "alegorias femininas da justiça"
+ ],
+ "source_query": "alegorias femininas da justiça"
+},
+{
+ "title": "Alegorias nos cemitérios do",
+ "authors": [
+  "RG do Sul"
+ ],
+ "year": 2008,
+ "venue": "RG do Sul - Cemitérios do Rio Grande do Sul: arte, sociedade …, 2008 - books.google.com",
+ "citation_count": 0,
+ "abstract": "… , femininas, que geralmente aparecem com um símbolo, podendo também aparecer com uma ação humana, ao qual damos o nome de alegorias. … Mas a justiça é a única que aparece …",
+ "url": "https://books.google.com/books?hl=zh-CN&lr=&id=-HH3Hkfo8o8C&oi=fnd&pg=PA113&dq=alegorias+femininas+da+justi%C3%A7a&ots=VMo4xQRc5H&sig=tQOhalXCCq8qgbyfORLbQLhIWZI",
+ "pdf_links": [],
+ "themes": [
+  "alegorias femininas da justiça"
+ ],
+ "source_query": "alegorias femininas da justiça"
+},
+{
+ "title": "Em busca de uma trajetória do visível: desenhos anarquistas e anticlericais ao redor do mundo",
+ "authors": [
+  "C Poletto"
+ ],
+ "year": 2015,
+ "venue": "C Poletto - XXVIII Simpósio Nacional de História, Florianópolis, 2015 - snh2015.anpuh.org",
+ "citation_count": 2,
+ "abstract": "… ; e a segunda diz respeito à utilização da alegoria feminina como porta-voz da Revolução … ideais de liberdade e justiça através da figura feminina. A alegoria feminina é utilizada para …",
+ "url": "http://snh2015.anpuh.org/resources/anais/39/1434413469_ARQUIVO_Embuscadeumatrajetoriadovisivel.pdf",
+ "pdf_links": [
+  "http://snh2015.anpuh.org/resources/anais/39/1434413469_ARQUIVO_Embuscadeumatrajetoriadovisivel.pdf"
+ ],
+ "themes": [
+  "alegorias femininas da justiça"
+ ],
+ "source_query": "alegorias femininas da justiça"
+},
+{
+ "title": "6. Diante Da Lei Tem Um Guarda: Acesso À Justiça Para Mulheres Em Situação De Violência Doméstica",
+ "authors": [
+  "F Simioni"
+ ],
+ "year": 2012,
+ "venue": "F Simioni - REVISTA DIÁLOGOS DO DIREITO-ISSN 2316-2112, 2012 - academia.edu",
+ "citation_count": 0,
+ "abstract": "… Se para uns a justiça é uma figura cega, na alegoria kafkaniana a justiça é representada por uma figura masculina, claudicante, que domina os ofícios e mistérios de uma crença em …",
+ "url": "https://www.academia.edu/download/60203156/27-1-807-1-10-20121118.pdf",
+ "pdf_links": [],
+ "themes": [
+  "alegorias femininas da justiça"
+ ],
+ "source_query": "alegorias femininas da justiça"
+},
+{
+ "title": "O direito patriarcal ea condição feminina",
+ "authors": [
+  "RXV Braga"
+ ],
+ "year": 2025,
+ "venue": "RXV Braga - 2025 - books.google.com",
+ "citation_count": 0,
+ "abstract": "… as alegorias da justiça serem representadas alegoricamente por figuras femininas enquanto … A alegoria é tanto um modo de expressão quanto de interpretação. Integra um processo …",
+ "url": "https://books.google.com/books?hl=zh-CN&lr=&id=zdOSEQAAQBAJ&oi=fnd&pg=PA7&dq=alegorias+femininas+da+justi%C3%A7a&ots=Yi6Gw0A1SM&sig=zBjWikIzXfASURskbD-KOXZogh8",
+ "pdf_links": [
+  "https://bibliotecadigital.stf.jus.br/xmlui/bitstream/handle/123456789/14637/in00001631520.pdf?sequence=1&isAllowed=y"
+ ],
+ "themes": [
+  "alegorias femininas da justiça"
+ ],
+ "source_query": "alegorias femininas da justiça"
+},
+{
+ "title": "Imaginário feminino-A representação das mulheres em Goya: uma análise de “Los Desastres de la Guerra”(cerca de 1810-1815)",
+ "authors": [
+  "LG Guidolin"
+ ],
+ "year": 2024,
+ "venue": "LG Guidolin - Humanidades em diálogo, 2024 - revistas.usp.br",
+ "citation_count": 1,
+ "abstract": "… o estudo das representações das guras femininas na série de gravuras, … a alegoria da Verdade – que, inclusive põem no campo de visão do espectador, a direita, a alegoria da Justiça, …",
+ "url": "https://revistas.usp.br/humanidades/article/view/212073",
+ "pdf_links": [
+  "https://revistas.usp.br/humanidades/article/download/212073/209580"
+ ],
+ "themes": [
+  "alegorias femininas da justiça"
+ ],
+ "source_query": "alegorias femininas da justiça"
+},
+{
+ "title": "A alegoria da Paz nas charges de J. Carlos durante a Primeira Grande Guerra: estilo, humor e narrativa.",
+ "authors": [
+  "DR DE SOUZA"
+ ],
+ "year": null,
+ "venue": "DR DE SOUZA - academia.edu",
+ "citation_count": 0,
+ "abstract": "… histórica dos conceitos bem como do uso da alegoria da Paz na História da Arte. Também … feminina, a alegoria da Paz incorporou ideais masculinos de uma civilidade feminina que …",
+ "url": "https://www.academia.edu/download/99302966/Dissertacao_Daniel_10_09_2021.pdf",
+ "pdf_links": [
+  "https://www.academia.edu/download/99302966/Dissertacao_Daniel_10_09_2021.pdf"
+ ],
+ "themes": [
+  "alegorias femininas da justiça"
+ ],
+ "source_query": "alegorias femininas da justiça"
+},
+{
+ "title": "Mística feminina na Idade Média–historiografia feminista e descolonização das paisagens medievais",
+ "authors": [
+  "L Troch"
+ ],
+ "year": 2012,
+ "venue": "L Troch - II SEMINÁRIO DE ESTUDOS MEDIEVAIS DA PARAÍBA …, 2012 - academia.edu",
+ "citation_count": 25,
+ "abstract": "… escrevem em forma de alegorias e discutem suas experiências … códigos de modo que se faça justiça às mulheres místicas que, ao … As alegorias ea poesia fornecem, ao mesmo tempo, …",
+ "url": "https://www.academia.edu/download/36150427/O_Calice_do_Diabo.pdf#page=27",
+ "pdf_links": [
+  "https://www.academia.edu/download/36150427/O_Calice_do_Diabo.pdf#page=27"
+ ],
+ "themes": [
+  "alegorias femininas da justiça"
+ ],
+ "source_query": "alegorias femininas da justiça"
+},
+{
+ "title": "Marianne, réflexions sur une histoire",
+ "authors": [
+  "M Agulhon"
+ ],
+ "year": 1992,
+ "venue": "M Agulhon - Annales historiques de la Révolution française, 1992 - JSTOR",
+ "citation_count": 15,
+ "abstract": "… histoire dès 1789, alors que le sobriquet de Marianne pour désigner la République n'est pas … C'est seulement à partir du Second Empire que le nom de Marianne et l'effigie républicaine …",
+ "url": "https://www.jstor.org/stable/41914292",
+ "pdf_links": [],
+ "themes": [
+  "Marianne / allegory of the Republic"
+ ],
+ "source_query": "Marianne allegory of the Republic"
+},
+{
+ "title": "Moses/Marianne parts the Red Sea: allegories of liberty in the bicentennial of the French Revolution",
+ "authors": [
+  "S Bonnemaison"
+ ],
+ "year": 1998,
+ "venue": "S Bonnemaison - Environment and Planning D: Society and …, 1998 - journals.sagepub.com",
+ "citation_count": 3,
+ "abstract": "… and proposes that allegory presents a possibility for redemptive … of Marianne—the traditional Republican allegory of French … a way as to displace the allegory of the Republic and Liberty. …",
+ "url": "https://journals.sagepub.com/doi/abs/10.1068/d160347",
+ "pdf_links": [],
+ "themes": [
+  "Marianne / allegory of the Republic"
+ ],
+ "source_query": "Marianne allegory of the Republic"
+},
+{
+ "title": "Gender, the Secular, and the Image of the Marianne in the French Revolution",
+ "authors": [
+  "KM Cherneski"
+ ],
+ "year": 2019,
+ "venue": "KM Cherneski - Past Imperfect, 2019 - journals.library.ualberta.ca",
+ "citation_count": 3,
+ "abstract": "… Revolution celebrated the Republic as an allegory of Liberty; the allegory took the twofold … the most enduring memories that the allegory left behind… allegories of flesh and blood might …",
+ "url": "https://journals.library.ualberta.ca/pi/index.php/pi/article/download/29348/21363",
+ "pdf_links": [
+  "https://journals.library.ualberta.ca/pi/index.php/pi/article/download/29348/21363"
+ ],
+ "themes": [
+  "Marianne / allegory of the Republic"
+ ],
+ "source_query": "Marianne allegory of the Republic"
+},
+{
+ "title": "Iconography of the Labour Movement. Part 1: Republican Iconography, 1792–1848",
+ "authors": [
+  "F Andersson"
+ ],
+ "year": 2020,
+ "venue": "F Andersson - ICO Iconographisk Post. Nordisk tidskrift för …, 2020 - journals.lub.lu.se",
+ "citation_count": 2,
+ "abstract": "… in later allegories of Liberty, Unity and the Republic: the crown of … He presents at least some evidence that “Marianne” might … belittle the Republic or Liberty: “She is a common Marianne”.…",
+ "url": "https://journals.lub.lu.se/ico/article/view/25660",
+ "pdf_links": [
+  "https://journals.lub.lu.se/ico/article/download/25660/22560"
+ ],
+ "themes": [
+  "Marianne / allegory of the Republic"
+ ],
+ "source_query": "Marianne allegory of the Republic"
+},
+{
+ "title": "Marianne into Battle: Republican Imagery and Symbolism in France, 1789-1880",
+ "authors": [
+  "AM Wagner"
+ ],
+ "year": 1983,
+ "venue": "AM Wagner - 1983 - JSTOR",
+ "citation_count": 1,
+ "abstract": "… The imagery of the first French republic, like the government which employed it, was rooted … the allegory \"Marianne\" was originally a derogatory move which saddled the republic with a …",
+ "url": "https://www.jstor.org/stable/1878620",
+ "pdf_links": [],
+ "themes": [
+  "Marianne / allegory of the Republic"
+ ],
+ "source_query": "Marianne allegory of the Republic"
+},
+{
+ "title": "Marianne au Combat: l'imagerie et la symbolique républicaine de 1789  1880",
+ "authors": [
+  "L Nochlin"
+ ],
+ "year": 1981,
+ "venue": "L Nochlin - 1981 - JSTOR",
+ "citation_count": 0,
+ "abstract": "… betwee artistic or literary creations and contemporary ideologies Nevertheless, his book constitutes an attempt to brin together, around the theme of the feminine allegory of th Republic, …",
+ "url": "https://www.jstor.org/stable/1360128",
+ "pdf_links": [],
+ "themes": [
+  "Marianne / allegory of the Republic"
+ ],
+ "source_query": "Marianne allegory of the Republic"
+},
+{
+ "title": "Allegorical and Emblematic Women's Faces",
+ "authors": [
+  "D Montandon"
+ ],
+ "year": 2021,
+ "venue": "D Montandon - Journal of Craniofacial Surgery, 2021 - journals.lww.com",
+ "citation_count": 0,
+ "abstract": "… of Marianne, became the symbol of the Republic. Since then, … Mariannes of the Republic, an allegory with 1000 visages,’’ … , everyday objects representing Marianne throughout its history…",
+ "url": "https://journals.lww.com/jcraniofacialsurgery/abstract/10.1097/scs.0000000000007124~allegorical-and-emblematic-womens-faces",
+ "pdf_links": [
+  "https://denysmontandon.com/wp-content/uploads/2022/01/Allegorical_and_Emblematic_Women_s_Faces..pdf"
+ ],
+ "themes": [
+  "Marianne / allegory of the Republic"
+ ],
+ "source_query": "Marianne allegory of the Republic"
+},
+{
+ "title": "Marianne into battle: Republican imagery and symbolism in France 1789–1880",
+ "authors": [
+  "D Outram"
+ ],
+ "year": 1982,
+ "venue": "D Outram - 1982 - Taylor & Francis",
+ "citation_count": 0,
+ "abstract": "… of ‘Marianne’ as the allegory of the Republic is also the … , ‘Marianne’ eventually became the property of the entire nation, which, in turn, had been prepared for the use of such an allegory …",
+ "url": "https://www.tandfonline.com/doi/pdf/10.1016/0191-6599(82)90025-0",
+ "pdf_links": [],
+ "themes": [
+  "Marianne / allegory of the Republic"
+ ],
+ "source_query": "Marianne allegory of the Republic"
+},
+{
+ "title": "Marianne in the Trenches: Typology and Iconographic Polysemy of Marianne between 1914 and 1918",
+ "authors": [
+  "L Bihl"
+ ],
+ "year": 2015,
+ "venue": "L Bihl - Humor, Entertainment, and Popular Culture during …, 2015 - Springer",
+ "citation_count": 0,
+ "abstract": "… , an allegory or a household name, Marianne goes down … allegory of the Republic for society, both in the combat zone and away from it. The text is printed alongside a naked Marianne …",
+ "url": "https://link.springer.com/chapter/10.1057/9781137436436_10",
+ "pdf_links": [],
+ "themes": [
+  "Marianne / allegory of the Republic"
+ ],
+ "source_query": "Marianne allegory of the Republic"
+},
+{
+ "title": "Marianne: from Medusa to Messalina: Psycho-sexual imagery and political propaganda in France 1789-1945",
+ "authors": [
+  "W Kidd"
+ ],
+ "year": 2004,
+ "venue": "W Kidd - Journal of European studies, 2004 - journals.sagepub.com",
+ "citation_count": 7,
+ "abstract": "… Republic, but was quite quickly displaced in favour of the now familiar female allegories: Marianne … version of the idealized, succouring allegory of official republican representation. She …",
+ "url": "https://journals.sagepub.com/doi/abs/10.1177/0047244104048702",
+ "pdf_links": [],
+ "themes": [
+  "Marianne / allegory of the Republic"
+ ],
+ "source_query": "Marianne allegory of the Republic"
+},
+{
+ "title": "Paradoxos carnavalescos: a presença feminina em carnavais da primeira república (1889-1910)",
+ "authors": [
+  "EB Nepomuceno"
+ ],
+ "year": 2013,
+ "venue": "EB Nepomuceno - CLIO-Revista de Pesquisa Histórica, 2013 - academia.edu",
+ "citation_count": 4,
+ "abstract": "… empenharam em associar a mulher à República, os membros das sociedades carnavalescas… se esforçado para colocar nas ruas a alegoria cívica feminina como símbolo da República. …",
+ "url": "https://www.academia.edu/download/36223687/CLIO_305-801-1-PB.pdf",
+ "pdf_links": [
+  "https://www.academia.edu/download/36223687/CLIO_305-801-1-PB.pdf"
+ ],
+ "themes": [
+  "República alegoria feminina"
+ ],
+ "source_query": "República alegoria feminina"
+},
+{
+ "title": "ENTRE MARIANNE E CLOTILDE: A REPRESENTAÇÃO FEMININA NAS REPRESENTAÇÕES DA REPÚBLICA BRASILEIRA (1889-1896)",
+ "authors": [
+  "FA de Sousa Nascimento…"
+ ],
+ "year": 2022,
+ "venue": "FA de Sousa Nascimento… - Revista de Estudos …, 2022 - ufs.emnuvens.com.br",
+ "citation_count": 0,
+ "abstract": "… Vale ressaltar que, as alegorias femininas utilizadas pelos positivistas brasileiros, foram … de alegoria feminina brasileira que possui nítida inspiração com as alegorias femininas da …",
+ "url": "https://ufs.emnuvens.com.br/revec/article/view/18508",
+ "pdf_links": [
+  "https://ufs.emnuvens.com.br/revec/article/download/18508/13406"
+ ],
+ "themes": [
+  "República alegoria feminina"
+ ],
+ "source_query": "República alegoria feminina"
+}
+]);
