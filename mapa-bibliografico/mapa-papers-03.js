2d74851c1e782d0aadc60af603e8d04ba19d303a
anavvanzin <ana@anavanzin.com>
2026-09-25 02:46:25 -0300

    feat(mapa): papers, parte 3/9

---
+++ mapa-bibliografico/mapa-papers-03.js
@@ -0,0 +1,390 @@
+window.MAPA_DATA.papers.push(...[
+{
+ "title": "Georges Didi-Huberman. L'image survivante: histoire de I'art et temps des fantomes selon Aby Warburg. Paris: Les Éditions de Minuit, 2002. 592 pp. index, illus. tbls …",
+ "authors": [
+  "H Farber"
+ ],
+ "year": 2004,
+ "venue": "H Farber - Renaissance Quarterly, 2004 - cambridge.org",
+ "citation_count": 3,
+ "abstract": "… This large study was written to present the full breadth of Aby Warburg's work and thought about the \"survival\" {Nachleben) of pictures, particularly in the very qualities that were …",
+ "url": "https://www.cambridge.org/core/services/aop-cambridge-core/content/view/C25D25FF237D7F7795D8CC224D62B38E/S0034433800068494a.pdf/georges_didihuberman_limage_survivante_histoire_de_iart_et_temps_des_fantomes_selon_aby_warburg_paris_les_editions_de_minuit_2002_592_pp_index_illus_tbls_bibl_27_isbn_2707317721.pdf",
+ "pdf_links": [
+  "https://journals.openedition.org/critiquedart/pdf/2162"
+ ],
+ "themes": [
+  "Warburg Nachleben"
+ ],
+ "source_query": "Warburg Nachleben survival of images"
+},
+{
+ "title": "The surviving image: Aby Warburg and Tylorian anthropology",
+ "authors": [
+  "G Didi-Huberman"
+ ],
+ "year": 2002,
+ "venue": "G Didi-Huberman - Oxford art journal, 2002 - JSTOR",
+ "citation_count": 135,
+ "abstract": "… How can we not recall the apex of the Nachleben der Antike, Warburg's analysis of the treatment of astrology in the Ferrara frescoes, or in Martin Luther's writings?25 In both cases and …",
+ "url": "https://www.jstor.org/stable/3600420",
+ "pdf_links": [],
+ "themes": [
+  "Warburg Nachleben"
+ ],
+ "source_query": "Warburg Nachleben survival of images"
+},
+{
+ "title": "Memory, metaphor, and Aby Warburg's Atlas of images",
+ "authors": [
+  "CD Johnson"
+ ],
+ "year": 2012,
+ "venue": "CD Johnson - 2012 - books.google.com",
+ "citation_count": 372,
+ "abstract": "… Nachleben der Antike (the afterlife of antiquity), it lends metaphor new historical and epistemological powers. Warburg … repertoire were, for Warburg, evidence of the survival of …",
+ "url": "https://books.google.com/books?hl=zh-CN&lr=&id=UKgCGCnUUvUC&oi=fnd&pg=PR7&dq=Warburg+Nachleben+survival+of+images&ots=dn1hQSKh9q&sig=ItIrmj-A57Yz5GVHI4sTt_bqXXQ",
+ "pdf_links": [],
+ "themes": [
+  "Warburg Nachleben"
+ ],
+ "source_query": "Warburg Nachleben survival of images"
+},
+{
+ "title": "The Surviving Image: Phantoms of Time and Time of Phantoms: Aby Warburg's History of Art by Georges Didi-Huberman",
+ "authors": [
+  "R Baxstrom"
+ ],
+ "year": 2017,
+ "venue": "R Baxstrom - 2017 - Taylor & Francis",
+ "citation_count": 0,
+ "abstract": "… Didi-Huberman is Nachleben (‘survival’), which broadly constitutes Warburg’s development … within the image that gives it ‘life’. Didi-Huberman correctly links Warburg’s Nachleben to …",
+ "url": "https://www.tandfonline.com/doi/full/10.1080/14714787.2017.1334385",
+ "pdf_links": [],
+ "themes": [
+  "Warburg Nachleben"
+ ],
+ "source_query": "Warburg Nachleben survival of images"
+},
+{
+ "title": "Translatio and Remediation: Aby Warburg, Image Migration and Photographic Reproduction",
+ "authors": [
+  "P Despoix"
+ ],
+ "year": 2015,
+ "venue": "P Despoix - SubStance, 2015 - muse.jhu.edu",
+ "citation_count": 7,
+ "abstract": "… is that in Warburg’s work, the concept of Nachleben—the … Warburg in a 1913 slide-lecture on the migration of planetary … spot, where transmitted texts and surviving forms of visual art …",
+ "url": "https://muse.jhu.edu/pub/19/article/588461/summary",
+ "pdf_links": [
+  "https://www.academia.edu/download/51574006/Translatio___Remediation_Substance_44.2-2015.pdf"
+ ],
+ "themes": [
+  "Warburg Nachleben"
+ ],
+ "source_query": "Warburg Nachleben survival of images"
+},
+{
+ "title": "Aby Warburg and the Afterlife of Antiquity: Morphology and History",
+ "authors": [
+  "C Cieri Via"
+ ],
+ "year": 2020,
+ "venue": "C Cieri Via - IKON, 2020 - brepolsonline.net",
+ "citation_count": 1,
+ "abstract": "… A formulation of the “Nachleben der Antike” by Aby Warburg … a morphological interest in images that differed both from the … Aby Warburg’s research on the great theme of the survival of …",
+ "url": "https://www.brepolsonline.net/content/journals/10.1484/J.IKON.5.121560",
+ "pdf_links": [],
+ "themes": [
+  "Warburg Nachleben"
+ ],
+ "source_query": "Warburg Nachleben survival of images"
+},
+{
+ "title": "2 After Afterlife Theorizing the Evolving Material Life of",
+ "authors": [
+  "D Karlholm"
+ ],
+ "year": 2026,
+ "venue": "D Karlholm - Materiality and The Afterlife of Artworks: The Rustle …, 2026 - books.google.com",
+ "citation_count": 0,
+ "abstract": "… translation of Aby Warburg’s notion Nachleben . For many, it … why a more common translation is survival. I will discuss both … ” is Warburg’s contention that “the powers of the image, both …",
+ "url": "https://books.google.com/books?hl=zh-CN&lr=&id=gFm_EQAAQBAJ&oi=fnd&pg=PT11&dq=Warburg+Nachleben+survival+of+images&ots=8nqfy15kX1&sig=VojRDPS93P9lFtf5BtVLGGK9nUc",
+ "pdf_links": [],
+ "themes": [
+  "Warburg Nachleben"
+ ],
+ "source_query": "Warburg Nachleben survival of images"
+},
+{
+ "title": "Nachlebende, Nachleben: Vehicles of Afterlife in (Some) Images and Words of the Warburg Library",
+ "authors": [
+  "K Mazzucco"
+ ],
+ "year": 2020,
+ "venue": "K Mazzucco - IKON, 2020 - brepolsonline.net",
+ "citation_count": 0,
+ "abstract": "… point of view on the question of translating Warburg’s language. The translations of Nachleben as Survival became canonical after the move of the Warburg Library to London, and is to …",
+ "url": "https://www.brepolsonline.net/content/journals/10.1484/J.IKON.5.121562",
+ "pdf_links": [],
+ "themes": [
+  "Warburg Nachleben"
+ ],
+ "source_query": "Warburg Nachleben survival of images"
+},
+{
+ "title": "Panofsky and the foundations of art history",
+ "authors": [
+  "MA Holly"
+ ],
+ "year": 1984,
+ "venue": "MA Holly - 1984 - books.google.com",
+ "citation_count": 602,
+ "abstract": "… Indeed, Panofsky's iconology offers a perfect model for the bridging of distinctions between different fields of historical research . Iconology does not unlock a painting or other repre…",
+ "url": "https://books.google.com/books?hl=zh-CN&lr=&id=ZQtYRQK0YI0C&oi=fnd&pg=PA9&dq=Panofsky+iconology&ots=iG0z8bEjs9&sig=E6qYhv1w5MAVg7w-NAhnEaFS3rg",
+ "pdf_links": [],
+ "themes": [
+  "Panofsky iconology"
+ ],
+ "source_query": "Panofsky iconology"
+},
+{
+ "title": "Panofsky, iconography, and semiotics",
+ "authors": [
+  "C Hasenmueller"
+ ],
+ "year": 1978,
+ "venue": "C Hasenmueller - The Journal of Aesthetics and Art Criticism, 1978 - JSTOR",
+ "citation_count": 197,
+ "abstract": "… terminology Panofsky used in distinguishing iconology … iconology was interpretation that went beyond the articulate he implied that the subject of iconography was articulate.14 Panofsky …",
+ "url": "https://www.jstor.org/stable/430439",
+ "pdf_links": [],
+ "themes": [
+  "Panofsky iconology"
+ ],
+ "source_query": "Panofsky iconology"
+},
+{
+ "title": "Iconography and iconology as a visual method and approach",
+ "authors": [
+  "MG Müller"
+ ],
+ "year": 2011,
+ "venue": "MG Müller - The SAGE handbook of visual research methods, 2011 - books.google.com",
+ "citation_count": 222,
+ "abstract": "… Panofsky himself did not give credit to his iconological … to link the iconological method only with Panofsky’s name. … To this day, Panofsky’s three steps constitute the core of iconology as …",
+ "url": "https://books.google.com/books?hl=zh-CN&lr=&id=AcBsjtkmPWkC&oi=fnd&pg=PA283&dq=Panofsky+iconology&ots=rR5Ph8JNSG&sig=V8117L7sDsfaHW5J18sBmKTJAmQ",
+ "pdf_links": [
+  "https://visualethnography.me/wp-content/uploads/2023/09/5632-Margolis-15.pdf"
+ ],
+ "themes": [
+  "Panofsky iconology"
+ ],
+ "source_query": "Panofsky iconology"
+},
+{
+ "title": "Panofsky's Concept of\" Iconology\" and the Problem of Interpretation in the History of Art",
+ "authors": [
+  "K Moxey"
+ ],
+ "year": 1986,
+ "venue": "K Moxey - New Literary History, 1986 - JSTOR",
+ "citation_count": 101,
+ "abstract": "… seen, had decisively colored Panofsky's scheme of \"iconological\" … in Panofsky's later work his conception of the \"iconological\" … of his definition of the \"iconological\" method. This level of …",
+ "url": "https://www.jstor.org/stable/468893",
+ "pdf_links": [],
+ "themes": [
+  "Panofsky iconology"
+ ],
+ "source_query": "Panofsky iconology"
+},
+{
+ "title": "The genesis of iconology",
+ "authors": [
+  "J Elsner",
+  "K Lorenz"
+ ],
+ "year": 2012,
+ "venue": "J Elsner, K Lorenz - Critical Inquiry, 2012 - journals.uchicago.edu",
+ "citation_count": 59,
+ "abstract": "… Erwin Panofsky explicitly states that the first half of the opening chapter of Studies in Iconology… Hamburg Ernst Cassirer, the principal interpreter of Kant in the 1920s, Panofsky from 1915 …",
+ "url": "https://www.journals.uchicago.edu/doi/full/10.1086/664548",
+ "pdf_links": [
+  "https://www.researchgate.net/profile/Jas-Elsner/publication/259710824_The_Genesis_of_Iconology/links/5e1d8d2392851c8364cbe26d/The-Genesis-of-Iconology.pdf"
+ ],
+ "themes": [
+  "Panofsky iconology"
+ ],
+ "source_query": "Panofsky iconology"
+},
+{
+ "title": "Panofsky and ICONCLASS",
+ "authors": [
+  "R Van Straten"
+ ],
+ "year": 1986,
+ "venue": "R Van Straten - Artibus et Historiae, 1986 - JSTOR",
+ "citation_count": 38,
+ "abstract": "… will first give a short description of Panofsky's scheme, then point out … Iconology as belonging to the field of Cultural History does not mean that art historians should not practice Iconology…",
+ "url": "https://www.jstor.org/stable/1483254",
+ "pdf_links": [
+  "https://iconclass.org/z/read/1986_van_straten.pdf"
+ ],
+ "themes": [
+  "Panofsky iconology"
+ ],
+ "source_query": "Panofsky iconology"
+},
+{
+ "title": "Ideology and iconology",
+ "authors": [
+  "GC Argan",
+  "R West"
+ ],
+ "year": 1975,
+ "venue": "GC Argan, R West - Critical Inquiry, 1975 - journals.uchicago.edu",
+ "citation_count": 112,
+ "abstract": "… Panofsky's iconological method has nothing to do with that … We must not forget that Panofsky built on the systematic study … by anyone who, like Panofsky, has seriously studied Cassirer's …",
+ "url": "https://www.journals.uchicago.edu/doi/pdf/10.1086/447841",
+ "pdf_links": [],
+ "themes": [
+  "Panofsky iconology"
+ ],
+ "source_query": "Panofsky iconology"
+},
+{
+ "title": "Iconography–iconology: Erwin Panofsky",
+ "authors": [
+  "M Hatt",
+  "C Klonk"
+ ],
+ "year": 2025,
+ "venue": "M Hatt, C Klonk - Art history, 2025 - manchesterhive.com",
+ "citation_count": 5,
+ "abstract": "… of art is only one aspect of Panofsky’s approach. He also … Panofsky described his method as having three stages: first, … matter; and third, an iconological analysis to show how the works …",
+ "url": "https://www.manchesterhive.com/abstract/9781526192325/9781526192325.00011.xml",
+ "pdf_links": [],
+ "themes": [
+  "Panofsky iconology"
+ ],
+ "source_query": "Panofsky iconology"
+},
+{
+ "title": "Panofsky: iconologia o semiologia",
+ "authors": [
+  "J Bialostocki"
+ ],
+ "year": 1981,
+ "venue": "J Bialostocki - Lettere italiane, 1981 - JSTOR",
+ "citation_count": 9,
+ "abstract": "… di Panofsky. Le ragioni di questa differenza sono varie, ma essa è dovuta soprattutto al fatto che Panofsky-in … i suoi scritti), le opere di Panofsky si leggevano come i migliori esempi della …",
+ "url": "https://www.jstor.org/stable/26260813",
+ "pdf_links": [],
+ "themes": [
+  "Panofsky iconology"
+ ],
+ "source_query": "Panofsky iconology"
+},
+{
+ "title": "ICONOLOGIE: Réflexions sur un concept d'Erwin Panofsky",
+ "authors": [
+  "B Teyssèdre"
+ ],
+ "year": 1964,
+ "venue": "B Teyssèdre - Revue Philosophique de la France et de l'Étranger, 1964 - JSTOR",
+ "citation_count": 10,
+ "abstract": "… Selon Panofsky, cette démarche double et unique se fonde, en son aspect esthétique, sur … la solution serait la meilleure possible ; mais Panofsky même pourrait enseigner à ne point se …",
+ "url": "https://www.jstor.org/stable/41090094",
+ "pdf_links": [],
+ "themes": [
+  "Panofsky iconology"
+ ],
+ "source_query": "Panofsky iconology"
+},
+{
+ "title": "Image, sujet, pouvoir. Entretien avec Marie-José Mondzain",
+ "authors": [
+  "M Fiserova",
+  "MJ Mondzain"
+ ],
+ "year": 2008,
+ "venue": "M Fiserova, MJ Mondzain - Sens public, 2008 - erudit.org",
+ "citation_count": 7,
+ "abstract": "… The interview with french philosopher Marie-José Mondzain, … image. As she is specialist of this topic, she devoted to it many of her books, especially {Image, icône, économie}, {L’image …",
+ "url": "https://www.erudit.org/en/journals/sp/2008-sp04857/1064391ar/abstract/",
+ "pdf_links": [
+  "https://www.erudit.org/en/journals/sp/2008-sp04857/1064391ar.pdf"
+ ],
+ "themes": [
+  "Mondzain image economy"
+ ],
+ "source_query": "Marie-José Mondzain image economy"
+},
+{
+ "title": "Visual empire",
+ "authors": [
+  "S Buck-Morss"
+ ],
+ "year": 2007,
+ "venue": "S Buck-Morss - diacritics, 2007 - muse.jhu.edu",
+ "citation_count": 60,
+ "abstract": "… Marie-José Mondzain makes a compelling argument for the … in the visual economy of the image.She describes the Christian … , nor does Mondzain, that the whole visual economy was a …",
+ "url": "https://muse.jhu.edu/pub/1/article/254740/summary",
+ "pdf_links": [],
+ "themes": [
+  "Mondzain image economy",
+  "political theology of images"
+ ],
+ "source_query": "Marie-José Mondzain image economy | political theology of images Mondzain"
+},
+{
+ "title": "Image, icon, economy: the Byzantine origins of the contemporary imaginary",
+ "authors": [
+  "MJ Mondzain"
+ ],
+ "year": 2005,
+ "venue": "MJ Mondzain - 2005 - books.google.com",
+ "citation_count": 391,
+ "abstract": "… of the image. But when and how was this fate sealed? Image, Icon, Economy considers this … images within the Western tradition. This book argues that the extraordinary force of the …",
+ "url": "https://books.google.com/books?hl=zh-CN&lr=&id=-aYQ2nDA6AAC&oi=fnd&pg=PP11&dq=Marie-Jos%C3%A9+Mondzain+image+economy&ots=zW7EeOFT8r&sig=gwvuipARccblIEMtigVDIm5VSxk",
+ "pdf_links": [],
+ "themes": [
+  "Mondzain image economy",
+  "political theology of images"
+ ],
+ "source_query": "Marie-José Mondzain image economy | political theology of images Mondzain"
+},
+{
+ "title": "Los intersticios de la imagen: economías de lo (in) visible e imaginación iconoclasta.",
+ "authors": [
+  "RB Dini"
+ ],
+ "year": 2024,
+ "venue": "RB Dini - Eikón/Imago, 2024 - openurl.ebsco.com",
+ "citation_count": 4,
+ "abstract": "… on the economy of the (in)visible, according to Marie-José … on the impact of images from the interdisciplinary crossroads … the Contemporary Imaginary, la filósofa Marie-José Mondzain …",
+ "url": "https://openurl.ebsco.com/contentitem/gcd:184907569?sid=ebsco:plink:crawler-gcd&id=ebsco:gcd:184907569&crl=c&jrnl=22548718",
+ "pdf_links": [
+  "https://tensionessuperficiales.com/wp-content/uploads/2024/09/90234.pdf"
+ ],
+ "themes": [
+  "Mondzain image economy"
+ ],
+ "source_query": "Marie-José Mondzain image economy"
+},
+{
+ "title": "Giving Credit to the Gaze",
+ "authors": [
+  "MJ Mondzain"
+ ],
+ "year": 2016,
+ "venue": "MJ Mondzain - Cinema, thought, life: conversation with Fata Morgana …, 2016 - torrossa.com",
+ "citation_count": 0,
+ "abstract": "… modern image (directly or indirectly linked to market economy… economy, by what or whoever holds the space of the image’s … , we have images that influence our freedom and images that …",
+ "url": "https://www.torrossa.com/gs/resourceProxy?an=4133763&publisher=FCS350",
+ "pdf_links": [],
+ "themes": [
+  "Mondzain image economy"
+ ],
+ "source_query": "Marie-José Mondzain image economy"
+}
+]);
