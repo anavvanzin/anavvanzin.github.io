043642d7b5c893583b52da00e056d5e91ef6e6b7
anavvanzin <ana@anavanzin.com>
2026-09-25 03:00:47 -0300

    feat(mapa): papers, parte 7/9

---
+++ mapa-bibliografico/mapa-papers-07.js
@@ -0,0 +1,374 @@
+window.MAPA_DATA.papers.push(...[
+{
+ "title": "\" O dia de amanhã\": a República nas páginas do periódico ilustrado O Mequetrefe, 1875-1889",
+ "authors": [
+  "AEM Lopes"
+ ],
+ "year": 2011,
+ "venue": "AEM Lopes - História (São Paulo), 2011 - SciELO Brasil",
+ "citation_count": 10,
+ "abstract": "… de governo no Brasil com a República. O objetivo principal deste … da República. Outro assunto a ser tratado aqui é como a República – por meio do emprego das alegorias femininas e …",
+ "url": "https://www.scielo.br/j/his/a/zkmbtDmGZfx6QkNYnrqwSxG/?lang=pt",
+ "pdf_links": [
+  "https://www.scielo.br/j/his/a/zkmbtDmGZfx6QkNYnrqwSxG/?format=pdf&lang=pt"
+ ],
+ "themes": [
+  "República alegoria feminina"
+ ],
+ "source_query": "República alegoria feminina"
+},
+{
+ "title": "O Brasil: um Estado-nação a ser contruído. O papel dos símbolos nacionais, do Império à República",
+ "authors": [
+  "J Jurt"
+ ],
+ "year": 2012,
+ "venue": "J Jurt - Mana, 2012 - SciELO Brasil",
+ "citation_count": 74,
+ "abstract": "… Segundo José Murilo de Carvalho, houve várias razões para que uma tradição de alegoria feminina da República não se desenvolvesse no Brasil. Por um lado, a função da mulher …",
+ "url": "https://www.scielo.br/j/mana/a/x47K6TgqwfrZ5CgPrPJDykk/?lang=pt",
+ "pdf_links": [
+  "https://www.scielo.br/j/mana/a/x47K6TgqwfrZ5CgPrPJDykk/?format=pdf&lang=pt"
+ ],
+ "themes": [
+  "República alegoria feminina"
+ ],
+ "source_query": "República alegoria feminina"
+},
+{
+ "title": "Marianne na Corte do Brasil: os periódicos ilustrados fluminenses e as imagens da alegoria feminina da república.",
+ "authors": [
+  "AEM Lopes"
+ ],
+ "year": null,
+ "venue": "AEM Lopes - encontro2008.rj.anpuh.org",
+ "citation_count": 0,
+ "abstract": "… O trabalho privilegia a alegoria feminina da República – Marianne – difundida a partir da República … Os artistas produziam nos jornais alegorias para abordar os republicanos sem se …",
+ "url": "http://encontro2008.rj.anpuh.org/resources/content/anais/1212845972_ARQUIVO_ArtigoAristeulopes.pdf",
+ "pdf_links": [
+  "http://encontro2008.rj.anpuh.org/resources/content/anais/1212845972_ARQUIVO_ArtigoAristeulopes.pdf"
+ ],
+ "themes": [
+  "República alegoria feminina"
+ ],
+ "source_query": "República alegoria feminina"
+},
+{
+ "title": "O Mequetrefe ea República: imprensa ilustrada, política e humor. Rio de Janeiro, século XIX",
+ "authors": [
+  "AEM Lopes"
+ ],
+ "year": 2011,
+ "venue": "AEM Lopes - 2011 - snh2011.anpuh.org",
+ "citation_count": 2,
+ "abstract": "… Essa ilustração é apenas uma entre tantas outras veiculadas nesse periódico sobre o conflito; ela é importante aos propósitos deste artigo por causa da alegoria feminina que foi …",
+ "url": "https://www.snh2011.anpuh.org/resources/anais/14/1300848493_ARQUIVO_TextoAristeuLopes.pdf",
+ "pdf_links": [
+  "https://www.snh2011.anpuh.org/resources/anais/14/1300848493_ARQUIVO_TextoAristeuLopes.pdf"
+ ],
+ "themes": [
+  "República alegoria feminina"
+ ],
+ "source_query": "República alegoria feminina"
+},
+{
+ "title": "A formação das almas: o imaginário da República no Brasil",
+ "authors": [
+  "JM De Carvalho"
+ ],
+ "year": 2023,
+ "venue": "JM De Carvalho - 2023 - books.google.com",
+ "citation_count": 3630,
+ "abstract": "… A alegoria feminina da República já era utilizada mesmo antes da proclamação; o barrete frígio aparecia invariavelmente, isolado ou cobrindo a cabeça da figura feminina… da república. …",
+ "url": "https://books.google.com/books?hl=zh-CN&lr=&id=kcWxEAAAQBAJ&oi=fnd&pg=PT7&dq=Rep%C3%BAblica+alegoria+feminina&ots=fJ8GqeIvN7&sig=FZqLxzyP6UWpzwsiIIpX9Gubpn0",
+ "pdf_links": [],
+ "themes": [
+  "República alegoria feminina"
+ ],
+ "source_query": "República alegoria feminina"
+},
+{
+ "title": "A república e seus símbolos: a imprensa ilustrada eo ideário republicano. Rio de Janeiro, 1868-1903",
+ "authors": [
+  "AEM Lopes"
+ ],
+ "year": 2010,
+ "venue": "AEM Lopes - 2010 - lume.ufrgs.br",
+ "citation_count": 4,
+ "abstract": "… Assim, no primeiro capítulo aparecerá a alegoria feminina da República, empregada para se referir à França; nos capítulos 2 e 3 ela aparece para tratar mais especificamente da …",
+ "url": "https://lume.ufrgs.br/handle/10183/23233",
+ "pdf_links": [
+  "https://lume.ufrgs.br/bitstream/handle/10183/23233/000738274.pdf?sequence=1"
+ ],
+ "themes": [
+  "República alegoria feminina"
+ ],
+ "source_query": "República alegoria feminina"
+},
+{
+ "title": "Marianne à brasileira: imagens republicanas e os dilemas do passado imperial",
+ "authors": [
+  "CR Lima Junior"
+ ],
+ "year": 2020,
+ "venue": "CR Lima Junior - 2020 - bdtd.ibict.br",
+ "citation_count": 3,
+ "abstract": "… supor, as imagens da República não suplantam totalmente as do … alegorias femininas da República, os retratos de Deodoro da Fonseca e as imagens da proclamação da República…",
+ "url": "https://bdtd.ibict.br/vufind/Record/USP_dffb42e89ee4fd85c522cbb295b8cfbf",
+ "pdf_links": [
+  "https://scholar.archive.org/work/jrnczfzkibcujdl4lvprw4toqa/access/wayback/https://teses.usp.br/teses/disponiveis/93/93131/tde-16062021-151518/publico/2020_CarlosRogerioLimaJunior_VCorr.pdf"
+ ],
+ "themes": [
+  "República alegoria feminina"
+ ],
+ "source_query": "República alegoria feminina"
+},
+{
+ "title": "Os símbolos da República nas páginas do humor: Política e imprensa ilustrada no Rio de Janeiro dos anos 1870",
+ "authors": [
+  "AM Lopes"
+ ],
+ "year": 2012,
+ "venue": "AM Lopes - Anos 90: Revista do Programa de Pós-Graduação em …, 2012 - redalyc.org",
+ "citation_count": 1,
+ "abstract": "… Com o auxílio das imagens sobre a campanha republicana, como aquelas que apresentam as alegorias femininas da República, é evidenciado que os periódicos ilustrados estavam …",
+ "url": "https://www.redalyc.org/pdf/5740/574069157010.pdf",
+ "pdf_links": [
+  "https://www.redalyc.org/pdf/5740/574069157010.pdf"
+ ],
+ "themes": [
+  "República alegoria feminina"
+ ],
+ "source_query": "República alegoria feminina"
+},
+{
+ "title": "Nachleben der Antike, Time, and Restitution: Notes for a Nocturnal Jurisprudence of the Image: I Stramignoni",
+ "authors": [
+  "I Stramignoni"
+ ],
+ "year": 2024,
+ "venue": "I Stramignoni - Law and Critique, 2024 - Springer",
+ "citation_count": 0,
+ "abstract": "… come from a “nocturnal” jurisprudence seeking to foreground its … a “nocturnal” jurisprudence of the image can be about. … towards what I propose to call a “nocturnal” jurisprudence of the …",
+ "url": "https://link.springer.com/article/10.1007/s10978-023-09371-4",
+ "pdf_links": [
+  "https://link.springer.com/content/pdf/10.1007/s10978-023-09371-4.pdf"
+ ],
+ "themes": [
+  "nocturnal jurisprudence (Stramignoni)"
+ ],
+ "source_query": "nocturnal jurisprudence Stramignoni"
+},
+{
+ "title": "Badiou's nocturnal jurisprudence",
+ "authors": [
+  "I Stramignoni"
+ ],
+ "year": 2007,
+ "venue": "I Stramignoni - Cardozo L. Rev., 2007 - HeinOnline",
+ "citation_count": 3,
+ "abstract": "… it today, and forget familiar forms of jurisprudence too. But long live a new nocturnal philosophy, long live a new nocturnal, subtractive jurisprudence, and its strange but intriguing non-…",
+ "url": "https://access.heinonline.com/hol-cgi-bin/get_pdf.cgi?handle=hein.journals/cdozo29&section=81",
+ "pdf_links": [
+  "https://papers.ssrn.com/sol3/Delivery.cfm?abstractid=1134343"
+ ],
+ "themes": [
+  "nocturnal jurisprudence (Stramignoni)"
+ ],
+ "source_query": "nocturnal jurisprudence Stramignoni"
+},
+{
+ "title": "Seizing Truths: Art, Politics, Law",
+ "authors": [
+  "I Stramignoni"
+ ],
+ "year": 2012,
+ "venue": "I Stramignoni - Law and Art, 2012 - api.taylorfrancis.com",
+ "citation_count": 5,
+ "abstract": "… of art of course but also that of ethics, jurisprudence, or law sink back into their embodied or … discourse, to a subtractive or nocturnal relationship between democracy as we know it, and …",
+ "url": "https://api.taylorfrancis.com/content/chapters/edit/download?identifierName=doi&identifierValue=10.4324/9780203816103-6&type=chapterpdf",
+ "pdf_links": [
+  "https://papers.ssrn.com/sol3/Delivery.cfm?abstractid=1873284"
+ ],
+ "themes": [
+  "nocturnal jurisprudence (Stramignoni)"
+ ],
+ "source_query": "nocturnal jurisprudence Stramignoni"
+},
+{
+ "title": "Figuring out justice in dark times: on law, history, and the visual",
+ "authors": [
+  "I Stramignoni"
+ ],
+ "year": 2023,
+ "venue": "I Stramignoni - Law and Humanities, 2023 - Taylor & Francis",
+ "citation_count": 1,
+ "abstract": "… -known physician, a compelling account of a nocturnal attempt to figure out justice at critical times, … There is a strange and intense quality to the nocturnal dialogue returned by our small …",
+ "url": "https://www.tandfonline.com/doi/abs/10.1080/17521483.2022.2139449",
+ "pdf_links": [
+  "https://www.tandfonline.com/doi/pdf/10.1080/17521483.2022.2139449"
+ ],
+ "themes": [
+  "nocturnal jurisprudence (Stramignoni)"
+ ],
+ "source_query": "nocturnal jurisprudence Stramignoni"
+},
+{
+ "title": "Illusion and betrayalThe city, the poets, or an ethics of truths?",
+ "authors": [
+  "I Stramignoni"
+ ],
+ "year": 2011,
+ "venue": "I Stramignoni - 2011 - account.utrechtlawreview.org",
+ "citation_count": 2,
+ "abstract": "… In jurisprudence critiques of the ideology of human rights range from C. Douzinas, The End … Stramignoni, ‘At the margins of the history of English law: the institutional, the socio-political, …",
+ "url": "https://account.utrechtlawreview.org/index.php/up-j-ulr/article/view/URN%3ANBN%3ANL%3AUI%3A10-1-101278",
+ "pdf_links": [
+  "https://account.utrechtlawreview.org/index.php/up-j-ulr/article/download/URN%3ANBN%3ANL%3AUI%3A10-1-101278/166"
+ ],
+ "themes": [
+  "nocturnal jurisprudence (Stramignoni)"
+ ],
+ "source_query": "nocturnal jurisprudence Stramignoni"
+},
+{
+ "title": "The Pasts of Law. Koselleck, Didi-Huberman and Sanmartino's Cristo Velato in Naples",
+ "authors": [
+  "I Stramignoni"
+ ],
+ "year": 2025,
+ "venue": "I Stramignoni - Law & Literature, 2025 - Taylor & Francis",
+ "citation_count": 0,
+ "abstract": "… Or consider intra-linguistic variations to do, for instance, with the vocabulary of theology, political philosophy, jurisprudence, art history or the rapidly developing scientific revolution – …",
+ "url": "https://www.tandfonline.com/doi/abs/10.1080/1535685X.2025.2600196",
+ "pdf_links": [
+  "https://www.tandfonline.com/doi/pdf/10.1080/1535685X.2025.2600196"
+ ],
+ "themes": [
+  "nocturnal jurisprudence (Stramignoni)"
+ ],
+ "source_query": "nocturnal jurisprudence Stramignoni"
+},
+{
+ "title": "Spacing (in) diaspora: law, literature and the Roma",
+ "authors": [
+  "E Patchett"
+ ],
+ "year": 2017,
+ "venue": "E Patchett - 2017 - books.google.com",
+ "citation_count": 7,
+ "abstract": "… a literary jurisprudence (Gurnham 2014, 12) of Diaspora space. A literary jurisprudence embraces … A literary jurisprudence provides a way to read space against a proprietorial doctrine, …",
+ "url": "https://books.google.com/books?hl=zh-CN&lr=&id=M3kqDwAAQBAJ&oi=fnd&pg=PP5&dq=nocturnal+jurisprudence+Stramignoni&ots=DKMj59Dzqm&sig=Ttmpn8J9p_CUgmwu0dIGYLcqWZM",
+ "pdf_links": [],
+ "themes": [
+  "nocturnal jurisprudence (Stramignoni)"
+ ],
+ "source_query": "nocturnal jurisprudence Stramignoni"
+},
+{
+ "title": "Franesco's Devilish Venus: Notations on the Matter of Legal Space",
+ "authors": [
+  "I Stramignoni"
+ ],
+ "year": 2004,
+ "venue": "I Stramignoni - Cal. WL Rev., 2004 - HeinOnline",
+ "citation_count": 31,
+ "abstract": "* Department of Law, London School of Economics and Political Science. A first, shorter version of this paper--drawing on ETA Hoffmann's The Devil's Elixirs to expose my notion of …",
+ "url": "https://access.heinonline.com/hol-cgi-bin/get_pdf.cgi?handle=hein.journals/cwlr41&section=8",
+ "pdf_links": [
+  "https://scholarlycommons.law.cwsl.edu/cgi/viewcontent.cgi?article=1136&context=cwlr"
+ ],
+ "themes": [
+  "nocturnal jurisprudence (Stramignoni)"
+ ],
+ "source_query": "nocturnal jurisprudence Stramignoni"
+},
+{
+ "title": "The spatial, the legal and the pragmatics of world-making: Nomospheric investigations",
+ "authors": [
+  "D Delaney"
+ ],
+ "year": 2010,
+ "venue": "D Delaney - 2010 - books.google.com",
+ "citation_count": 548,
+ "abstract": "… However, as Igor Stramignoni has observed, \"in spite of the ubiquity of Space in law's many legal domains or, perhaps, precisely because of that ubiquity—modern Western law seems …",
+ "url": "https://books.google.com/books?hl=zh-CN&lr=&id=0y8vCgAAQBAJ&oi=fnd&pg=PP1&dq=nocturnal+jurisprudence+Stramignoni&ots=fjlFviAtIC&sig=z-ZpJCJXPoT0ahfJj1L1nhJ2LYU",
+ "pdf_links": [],
+ "themes": [
+  "nocturnal jurisprudence (Stramignoni)"
+ ],
+ "source_query": "nocturnal jurisprudence Stramignoni"
+},
+{
+ "title": "The Idea of Terrorism in China",
+ "authors": [
+  "KC Wong"
+ ],
+ "year": 2010,
+ "venue": "KC Wong - Pakistan Journal of Criminology, 2010 - pscriminology.com",
+ "citation_count": 4,
+ "abstract": "This research investigated an old political problem in a new cultural context: what is the idea of terrorism in China? Specifically, this research posed two inter-related research questions …",
+ "url": "http://pscriminology.com/wp-content/uploads/2018/01/PJCVol2No1Jan2010.pdf#page=58",
+ "pdf_links": [
+  "http://pscriminology.com/wp-content/uploads/2018/01/PJCVol2No1Jan2010.pdf#page=58"
+ ],
+ "themes": [
+  "nocturnal jurisprudence (Stramignoni)"
+ ],
+ "source_query": "nocturnal jurisprudence Stramignoni"
+},
+{
+ "title": "Emergence and personification: How the law makes non-human entities speak",
+ "authors": [
+  "G Teubner"
+ ],
+ "year": 2026,
+ "venue": "G Teubner - Research Handbook on Law and Systems Theory, 2026 - elgaronline.com",
+ "citation_count": 6,
+ "abstract": "… personification creates in the law an independent centre of activity with internal and external effects. The personification … ” mute events external to the law into eloquent legal acts. In …",
+ "url": "https://www.elgaronline.com/abstract/book/9781035336494/chapter3.xml",
+ "pdf_links": [
+  "https://www.helsinki.fi/assets/drupal/2025-08/TeubnerEmergenceAndPersonificationHelsinki2025.pdf"
+ ],
+ "themes": [
+  "personification of law"
+ ],
+ "source_query": "personification of law"
+},
+{
+ "title": "In the hall of masks: Contrasting modes of personification",
+ "authors": [
+  "N Van Dijk"
+ ],
+ "year": 2020,
+ "venue": "N Van Dijk - Life and the law in the era of data-driven agency, 2020 - elgaronline.com",
+ "citation_count": 8,
+ "abstract": "… Against this it has been argued that in law, ‘personification’, the act of qualifying someone or … Such entities have a fictive mode of existence in law. We should be clear here in what we …",
+ "url": "https://www.elgaronline.com/abstract/edcoll/9781788971997/9781788971997.00020.xml",
+ "pdf_links": [
+  "https://www.academia.edu/download/64095430/van_Dijk_-_In_the_Hall_of_Masks._Contrasting_Modes_of_Personification.pdf"
+ ],
+ "themes": [
+  "personification of law"
+ ],
+ "source_query": "personification of law"
+},
+{
+ "title": "Personification in Private Law",
+ "authors": [
+  "M Oza"
+ ],
+ "year": 2026,
+ "venue": "M Oza - … Eva Micheler, eds., Legal Personhood in Private Law, 2026 - papers.ssrn.com",
+ "citation_count": 0,
+ "abstract": "… I illustrate this with case law on the personality of idols. In Section IV, I argue that the effect of personification is to confer authority on an individual or group to make decisions with …",
+ "url": "https://papers.ssrn.com/sol3/papers.cfm?abstract_id=6451258",
+ "pdf_links": [
+  "https://papers.ssrn.com/sol3/Delivery.cfm?abstractid=6451258"
+ ],
+ "themes": [
+  "personification of law"
+ ],
+ "source_query": "personification of law"
+}
+]);
