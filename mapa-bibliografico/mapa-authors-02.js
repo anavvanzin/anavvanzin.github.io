2a30e21ef713d6562fc46f592241820a420b6441
anavvanzin <ana@anavanzin.com>
2026-09-25 02:38:53 -0300

    feat(mapa): autores-chave, parte 2 (Didi-Huberman, Stramignoni, Dal Ri)

---
+++ mapa-bibliografico/mapa-authors-02.js
@@ -0,0 +1,262 @@
+window.MAPA_DATA.authors.push(...[
+{
+ "author_name": "Georges Didi-Huberman",
+ "queried_as": "Georges Didi-Huberman",
+ "affiliation": "books.google.com",
+ "h_index": 10,
+ "i10_index": 10,
+ "total_citations": 3671,
+ "publications_count": 10,
+ "career_span": "1984-2018",
+ "research_interests": [
+  "Before",
+  "People"
+ ],
+ "top_papers": [
+  {
+   "title": "Images in spite of all: Four photographs from Auschwitz",
+   "year": 2008,
+   "venue": "2008",
+   "citation_count": 1027,
+   "url": "https://books.google.com/books?hl=zh-CN&lr=&id=CXuNvssxILIC&oi=fnd&pg=PP6&dq=%22Georges+Didi-Huberman%22&ots=d7YPlQc0NJ&sig=4zT4eJDjDE1USEaMBFUpevf9gVY"
+  },
+  {
+   "title": "Invention of hysteria: Charcot and the photographic iconography of the Salpêtrière",
+   "year": 2004,
+   "venue": "2004",
+   "citation_count": 783,
+   "url": "https://books.google.com/books?hl=zh-CN&lr=&id=uLtNEAAAQBAJ&oi=fnd&pg=PA14&dq=%22Georges+Didi-Huberman%22&ots=9DrzizSqDq&sig=3F2tyyVhhH1cCP5H1rCf7p-aKo4"
+  },
+  {
+   "title": "Confronting images: Questioning the ends of a certain history of art",
+   "year": 2005,
+   "venue": "2005",
+   "citation_count": 647,
+   "url": "https://books.google.com/books?hl=zh-CN&lr=&id=WXRB5AJrSd4C&oi=fnd&pg=PR10&dq=%22Georges+Didi-Huberman%22&ots=4TfstL2LOb&sig=UTU9wpZx3_4aYdDu44MamYwZdAg"
+  },
+  {
+   "title": "Fra Angelico: dissemblance and figuration",
+   "year": 1995,
+   "venue": "1995",
+   "citation_count": 546,
+   "url": "https://books.google.com/books?hl=zh-CN&lr=&id=yQNRdLBxq2kC&oi=fnd&pg=PR9&dq=%22Georges+Didi-Huberman%22&ots=KGqMXQtkaP&sig=xsizrsx9p5NaimJnMLC-TQuCeH8"
+  },
+  {
+   "title": "Before the image, before time: the sovereignty of anachronism",
+   "year": 2003,
+   "venue": "Compelling visuality: The work of art in and …, 2003",
+   "citation_count": 154,
+   "url": "https://books.google.com/books?hl=zh-CN&lr=&id=WeE5gvAQHWgC&oi=fnd&pg=PA31&dq=%22Georges+Didi-Huberman%22&ots=nKSHlCAhzi&sig=7xLEWSi0xBxT5b9NSmkw6Hm9__A"
+  },
+  {
+   "title": "Artistic survival: Panofsky vs. Warburg and the exorcism of impure time",
+   "year": 2003,
+   "venue": "Common Knowledge, 2003",
+   "citation_count": 151,
+   "url": "https://muse.jhu.edu/pub/4/article/41437/summary"
+  },
+  {
+   "title": "The index of the absent wound (monograph on a stain)",
+   "year": 1984,
+   "venue": "October, 1984",
+   "citation_count": 117,
+   "url": "https://www.jstor.org/stable/778307"
+  },
+  {
+   "title": "Survival of the Fireflies",
+   "year": 2018,
+   "venue": "2018",
+   "citation_count": 117,
+   "url": "https://books.google.com/books?hl=zh-CN&lr=&id=sCp0DwAAQBAJ&oi=fnd&pg=PA6&dq=%22Georges+Didi-Huberman%22&ots=nRta6ZfJu8&sig=AFTOkcj0OJxadIObsMioc8C-6K8"
+  },
+  {
+   "title": "Atlas, or the anxious gay science",
+   "year": 2018,
+   "venue": "2018",
+   "citation_count": 91,
+   "url": "https://books.google.com/books?hl=zh-CN&lr=&id=FXlvDwAAQBAJ&oi=fnd&pg=PR2&dq=%22Georges+Didi-Huberman%22&ots=zzzF9DTYuo&sig=pwW2NXC3Gn12GqKX_1DFVx1wQVY"
+  },
+  {
+   "title": "People exposed, people as extras",
+   "year": 2009,
+   "venue": "Radical Philosophy, 2009",
+   "citation_count": 38,
+   "url": "https://www.radicalphilosophyarchive.com/issue-files/rp156_article2_peopleexposed_huberman.pdf"
+  }
+ ]
+},
+{
+ "author_name": "Igor Stramignoni",
+ "queried_as": "Igor Stramignoni",
+ "affiliation": "HeinOnline",
+ "h_index": 5,
+ "i10_index": 4,
+ "total_citations": 86,
+ "publications_count": 10,
+ "career_span": "2002-2025",
+ "research_interests": [
+  "Comparative"
+ ],
+ "top_papers": [
+  {
+   "title": "Franesco's Devilish Venus: Notations on the Matter of Legal Space",
+   "year": 2004,
+   "venue": "Cal. WL Rev., 2004",
+   "citation_count": 31,
+   "url": "https://access.heinonline.com/hol-cgi-bin/get_pdf.cgi?handle=hein.journals/cwlr41&section=8"
+  },
+  {
+   "title": "The King's One Too Many Eyes: Language, Thought, and Comparative Law",
+   "year": 2002,
+   "venue": "Utah L. Rev., 2002",
+   "citation_count": 16,
+   "url": "https://access.heinonline.com/hol-cgi-bin/get_pdf.cgi?handle=hein.journals/utahlr2002&section=31"
+  },
+  {
+   "title": "Meditating Comparisons, or the Question of Comparative Law",
+   "year": 2003,
+   "venue": "San Diego Int'l LJ, 2003",
+   "citation_count": 13,
+   "url": "https://access.heinonline.com/hol-cgi-bin/get_pdf.cgi?handle=hein.journals/sdintl4&section=7"
+  },
+  {
+   "title": "At the margins of the history of English law: the institutional, the socio-political and the 'blotted-out'",
+   "year": 2002,
+   "venue": "Legal Studies, 2002",
+   "citation_count": 12,
+   "url": "https://www.cambridge.org/core/journals/legal-studies/article/at-the-margins-of-the-history-of-english-law-the-institutional-the-sociopolitical-and-the-blottedout/2DC2C8BF8F09EE9577B9320D91F45E40"
+  },
+  {
+   "title": "Seizing Truths: Art, Politics, Law",
+   "year": 2012,
+   "venue": "Law and Art, 2012",
+   "citation_count": 5,
+   "url": "https://api.taylorfrancis.com/content/chapters/edit/download?identifierName=doi&identifierValue=10.4324/9780203816103-6&type=chapterpdf"
+  },
+  {
+   "title": "Le Regard de La Comparaison: Nietzsche, Heidegger, Derrida (The Gaze of Comparison: Nietzsche, Heidegger, Derrida)",
+   "year": 2009,
+   "venue": "Comparer les droits, résolument, 2009",
+   "citation_count": 3,
+   "url": "https://papers.ssrn.com/sol3/papers.cfm?abstract_id=1825816"
+  },
+  {
+   "title": "Badiou's nocturnal jurisprudence",
+   "year": 2007,
+   "venue": "Cardozo L. Rev., 2007",
+   "citation_count": 3,
+   "url": "https://access.heinonline.com/hol-cgi-bin/get_pdf.cgi?handle=hein.journals/cdozo29&section=81"
+  },
+  {
+   "title": "Illusion and betrayalThe city, the poets, or an ethics of truths?",
+   "year": 2011,
+   "venue": "2011",
+   "citation_count": 2,
+   "url": "https://account.utrechtlawreview.org/index.php/up-j-ulr/article/view/URN%3ANBN%3ANL%3AUI%3A10-1-101278"
+  },
+  {
+   "title": "Figuring out justice in dark times: on law, history, and the visual",
+   "year": 2023,
+   "venue": "Law and Humanities, 2023",
+   "citation_count": 1,
+   "url": "https://www.tandfonline.com/doi/abs/10.1080/17521483.2022.2139449"
+  },
+  {
+   "title": "The Pasts of Law. Koselleck, Didi-Huberman and Sanmartino's Cristo Velato in Naples",
+   "year": 2025,
+   "venue": "Law & Literature, 2025",
+   "citation_count": 0,
+   "url": "https://www.tandfonline.com/doi/abs/10.1080/1535685X.2025.2600196"
+  }
+ ]
+},
+{
+ "author_name": "Arno Dal Ri",
+ "queried_as": "Arno Dal Ri Jr.",
+ "affiliation": "rivisteopen.unimc.it",
+ "h_index": 3,
+ "i10_index": 3,
+ "total_citations": 574,
+ "publications_count": 10,
+ "career_span": "2004-2025",
+ "research_interests": [
+  "International",
+  "Reception",
+  "Principle",
+  "Nationalities",
+  "Derecho"
+ ],
+ "top_papers": [
+  {
+   "title": "Mitologias jurídicas da modernidade",
+   "year": 2004,
+   "venue": "2004",
+   "citation_count": 520,
+   "url": "https://www.academia.edu/download/62243154/Mitologias_Juridicas_da_Modernidade_2ed.pdf"
+  },
+  {
+   "title": "Derecho en movimiento. Personas, derechos y derecho en la dinámica global",
+   "year": 2015,
+   "venue": "2015",
+   "citation_count": 40,
+   "url": "https://e-archivo.uc3m.es/entities/publication/1f116bf2-518a-4b54-9a67-b70024abedfd"
+  },
+  {
+   "title": "Transnational Corporations Subjectivity Based on the Criteria of the Bernadotte Case and the Traditional International Law Doctrine",
+   "year": 2018,
+   "venue": "Anuario mexicano de derecho …, 2018",
+   "citation_count": 11,
+   "url": "https://www.scielo.org.mx/scielo.php?pid=S1870-46542018000100155&script=sci_arttext&tlng=en"
+  },
+  {
+   "title": "Mancini in the Platine Basin: The Reception of the Principle of Nationalities in Argentina, Uruguay and Brazil",
+   "year": 2018,
+   "venue": "2018",
+   "citation_count": 2,
+   "url": "https://www.academia.edu/download/60094631/Mancini_in_the_Platine_Basin__The_Reception_of_the_Principle_of_Nationalities_in_Argentina__Uruguay_and_Brazil20190723-43330-1w19q25.pdf"
+  },
+  {
+   "title": "Reception of norms of international law in the Brazilian Constitutional Experience: Doctrinal Conceptions about Executive Power overlays Between the Empire and the …",
+   "year": 2020,
+   "venue": "Giornale di Storia Costituzionale, 2020",
+   "citation_count": 1,
+   "url": "https://access.heinonline.com/hol-cgi-bin/get_pdf.cgi?handle=hein.journals/giordi40&section=10"
+  },
+  {
+   "title": "Recezione delle norme del diritto internazionale nell'esperienza costituzionale brasiliana: concezioni dottrinali sulle sovrapposizioni del potere esecutivo tra Impero e …",
+   "year": 2020,
+   "venue": "Giornale di storia costituzionale, 2020",
+   "citation_count": 0,
+   "url": "https://rivisteopen.unimc.it/index.php/gsc/article/view/4441"
+  },
+  {
+   "title": "O Ordenamento Jurídico Italiano enquanto Laboratório de Combate aos Movimentos Neofascistas: Experiências na Construção de Mecanismos de Proteção da …",
+   "year": 2024,
+   "venue": "Direito Público, 2024",
+   "citation_count": 0,
+   "url": "https://www.portaldeperiodicos.idp.edu.br/direitopublico/article/view/7840"
+  },
+  {
+   "title": "François Laurent in Brazil. The defense of the Principle of Nationalities that crossed the Atlantic (1899-1978)",
+   "year": 2025,
+   "venue": "Sartoniana, 2025",
+   "citation_count": 0,
+   "url": "https://openjournals.ugent.be/sartoniana/article/id/101334/download/pdf/"
+  },
+  {
+   "title": "CONTRADIÇÕES ITALIANAS NOS BASTIDORES DO TRATADO DE SAINT-GERMAIN-EN-LAYE (1919): O PRINCÍPIO DAS NACIONALIDADES EA …",
+   "year": null,
+   "venue": "revistas.ufpr.br",
+   "citation_count": 0,
+   "url": "https://revistas.ufpr.br/direito/article/download/97213/75061"
+  },
+  {
+   "title": "INTERNATIONAL ENVIRONMENTAL LAW",
+   "year": 2025,
+   "venue": "Journal of International Law, 2025",
+   "citation_count": 0,
+   "url": "https://www.researchgate.net/profile/Arno-Dal-Ri-Jr/publication/393975886_ozdanie_Ramocnoj_konvencii_Organizacii_Obedinennyh_Nacij_ob_izmenenii_klimata_kak_osnovy_mezdunarodnogo_pravovogo_rezima_v_oblasti_klimata_1985-1992_gg_The_Establishment_of_the_United_Nations_Framewor/links/688233ea4eccfb3f29c4b96d/ozdanie-Ramocnoj-konvencii-Organizacii-Obedinennyh-Nacij-ob-izmenenii-klimata-kak-osnovy-mezdunarodnogo-pravovogo-rezima-v-oblasti-klimata-1985-1992-gg-The-Establishment-of-the-United-Nations-Framew.pdf"
+  }
+ ]
+}
+]);
