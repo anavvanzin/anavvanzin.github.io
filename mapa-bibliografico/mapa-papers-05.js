8daf2c1cfa4bf2dd1a2d4bd39e78ace6a867ff1d
anavvanzin <ana@anavanzin.com>
2026-09-25 02:55:25 -0300

    feat(mapa): papers, parte 5/9

---
+++ mapa-bibliografico/mapa-papers-05.js
@@ -0,0 +1,388 @@
+window.MAPA_DATA.papers.push(...[
+{
+ "title": "O Conceito de\" Partilha do Sensível\" e as suas Implicações no que Concerne a Atividade Política em Jacques Rancière",
+ "authors": [
+  "MA Teixeira"
+ ],
+ "year": 2022,
+ "venue": "MA Teixeira - Revista de Filosofia Moderna e Contemporânea, 2022 - periodicos.unb.br",
+ "citation_count": 1,
+ "abstract": "… partilha do sensível na teoria de Jacques Rancière. Com o objetivo de compreendermos a gênese da noção de partilha do sensível … A partilha do sensível seria um processo político e …",
+ "url": "https://periodicos.unb.br/index.php/fmc/article/view/47774",
+ "pdf_links": [
+  "https://periodicos.unb.br/index.php/fmc/article/download/47774/36540"
+ ],
+ "themes": [
+  "partilha do sensível"
+ ],
+ "source_query": "partilha do sensível Rancière"
+},
+{
+ "title": "Momentos estéticos: Rancière ea política da arte",
+ "authors": [
+  "JP Cachopo"
+ ],
+ "year": 2013,
+ "venue": "JP Cachopo - Aisthe, 2013 - academia.edu",
+ "citation_count": 16,
+ "abstract": "… relação com o conceito de “partilha do sensível” à crítica da “… 2 Nas palavras de Rancière: “Toma-se aqui partilha [partage] … de uma com a outra que define uma partilha do sensível.” (…",
+ "url": "https://www.academia.edu/download/37797790/Momentos_Esteticos_AISTHE.pdf",
+ "pdf_links": [
+  "https://www.academia.edu/download/37797790/Momentos_Esteticos_AISTHE.pdf"
+ ],
+ "themes": [
+  "partilha do sensível"
+ ],
+ "source_query": "partilha do sensível Rancière"
+},
+{
+ "title": "Reconhecibilidade, percepção ea partilha do sensível: Honneth, Rancière e Butler",
+ "authors": [
+  "D Petherbridge",
+  "AS de Melo",
+  "GVA Ruiz…"
+ ],
+ "year": 2020,
+ "venue": "D Petherbridge, AS de Melo, GVA Ruiz… - Cadernos de Filosofia …, 2020 - revistas.usp.br",
+ "citation_count": 3,
+ "abstract": "… da forma sensível e da forma “estética”. Com isso, a ordem política é conceitualizada nos termos do que Rancière se refere como a partage du sensible, a partilha do sensível. Essa …",
+ "url": "https://revistas.usp.br/filosofiaalema/article/view/176242",
+ "pdf_links": [
+  "https://revistas.usp.br/filosofiaalema/article/download/176242/167075"
+ ],
+ "themes": [
+  "partilha do sensível"
+ ],
+ "source_query": "partilha do sensível Rancière"
+},
+{
+ "title": "Literatura e política em Jacques Rancière: a partilha do sensível eo vislumbre da emancipação",
+ "authors": [
+  "MA Teixeira"
+ ],
+ "year": 2025,
+ "venue": "MA Teixeira - Revista de Filosofia Moderna e Contemporânea, 2025 - periodicos.unb.br",
+ "citation_count": 0,
+ "abstract": "… A “partilha do sensível” é um conceito que fundamenta a teoria rancie… da “partilha do sensível” defendida por Rancière, é aprofundar o escopo da reconfiguração da partilha do sensível …",
+ "url": "https://periodicos.unb.br/index.php/fmc/article/view/56057",
+ "pdf_links": [
+  "https://periodicos.unb.br/index.php/fmc/article/download/56057/43435"
+ ],
+ "themes": [
+  "partilha do sensível"
+ ],
+ "source_query": "partilha do sensível Rancière"
+},
+{
+ "title": "Blind justice",
+ "authors": [
+  "IB Capers"
+ ],
+ "year": 2012,
+ "venue": "IB Capers - Yale JL & Human., 2012 - HeinOnline",
+ "citation_count": 29,
+ "abstract": "… similar point in his Procedure: There is a critical ambiguity to the blindfold of Justice-which … Representing Justice has prompted me to give Justitia a second look. Again, it is her blindfold …",
+ "url": "https://access.heinonline.com/hol-cgi-bin/get_pdf.cgi?handle=hein.journals/yallh24&section=11",
+ "pdf_links": [
+  "https://brooklynworks.brooklaw.edu/cgi/viewcontent.cgi?article=1114&context=faculty"
+ ],
+ "themes": [
+  "blindfolded justice"
+ ],
+ "source_query": "blindfolded justice | blindfolded justice"
+},
+{
+ "title": "The blind justice paradox: Judges with visual impairments and the disability metaphor",
+ "authors": [
+  "D Dorfman"
+ ],
+ "year": 2016,
+ "venue": "D Dorfman - Cambridge International Law Journal, 2016 - elgaronline.com",
+ "citation_count": 19,
+ "abstract": "… through the image of a blindfolded woman who represents justice. Nonetheless, not much has … Nor has the ‘Icon of Justice’ been used to represent the social struggle for disability rights. …",
+ "url": "https://www.elgaronline.com/view/journals/cilj/5-2/cilj.2016.02.05.xml?",
+ "pdf_links": [
+  "https://papers.ssrn.com/sol3/Delivery.cfm?abstractid=2935540"
+ ],
+ "themes": [
+  "blindfolded justice"
+ ],
+ "source_query": "blindfolded justice"
+},
+{
+ "title": "Law's blindfold",
+ "authors": [
+  "D Luban"
+ ],
+ "year": 2001,
+ "venue": "D Luban - Conflict of interest in the professions, 2001 - discuss.tp4.ir",
+ "citation_count": 16,
+ "abstract": "… regarded the blindfold as an unqualified good. Justice blindfolded can be justice hoodwinked—a … A 1554 painting by Pieter Brueghel depicts blind justice surrounded by a panorama of …",
+ "url": "https://discuss.tp4.ir/uploads/short-url/8AnMHV80A1B3dwnNVxxJmqQY2Zz.pdf#page=32",
+ "pdf_links": [
+  "https://discuss.tp4.ir/uploads/short-url/8AnMHV80A1B3dwnNVxxJmqQY2Zz.pdf#page=32"
+ ],
+ "themes": [
+  "blindfolded justice"
+ ],
+ "source_query": "blindfolded justice"
+},
+{
+ "title": "The Foolosophy of justice and the Enigma of law",
+ "authors": [
+  "P Goodrich"
+ ],
+ "year": 2012,
+ "venue": "P Goodrich - Yale JL & Human., 2012 - HeinOnline",
+ "citation_count": 19,
+ "abstract": "… Specifically, when legal texts print pictures of Justice and of Justice blindfolded, as they did … the proper interpretation of the blindfold, which we find not only on Justice (Justitia) but also …",
+ "url": "https://access.heinonline.com/hol-cgi-bin/get_pdf.cgi?handle=hein.journals/yallh24&section=10",
+ "pdf_links": [
+  "https://larc.cardozo.yu.edu/cgi/viewcontent.cgi?article=2312&context=faculty-articles"
+ ],
+ "themes": [
+  "Peter Goodrich legal emblems",
+  "blindfolded justice"
+ ],
+ "source_query": "blindfolded justice | Goodrich legal emblems"
+},
+{
+ "title": "The paradoxes of lady justice's blindfold",
+ "authors": [
+  "V Hayaert"
+ ],
+ "year": 2018,
+ "venue": "V Hayaert - … and Iconography of Law and Justice in Context, from …, 2018 - Springer",
+ "citation_count": 7,
+ "abstract": "… He has an original statement about the blindfold of Justice: ‘We say that, even if Justice is blindfolded, she is not blind. As when she’s seeking truth (which is the task of the investigation …",
+ "url": "https://link.springer.com/chapter/10.1007/978-3-319-90787-1_11",
+ "pdf_links": [
+  "https://www.academia.edu/download/113715970/978-3-319-90787-1.pdf#page=213"
+ ],
+ "themes": [
+  "blindfolded justice"
+ ],
+ "source_query": "blindfolded justice"
+},
+{
+ "title": "Must justice be blind? The challenge of images to the law",
+ "authors": [
+  "M Jay"
+ ],
+ "year": 1996,
+ "venue": "M Jay - Filozofski vestnik, 1996 - ojs.zrc-sazu.si",
+ "citation_count": 6,
+ "abstract": "… « A blindfolded justice could thus avoid the seductions of images and achieve the … private, feudal justice. It was not by chance that many statues or fountains of blindfolded Justitia were …",
+ "url": "https://ojs.zrc-sazu.si/filozofski-vestnik/article/view/3951",
+ "pdf_links": [
+  "https://ojs.zrc-sazu.si/filozofski-vestnik/article/download/3951/3661"
+ ],
+ "themes": [
+  "blindfolded justice"
+ ],
+ "source_query": "blindfolded justice"
+},
+{
+ "title": "Epistemological doubt and visual puzzles of sight, knowledge and judgment: reflections on clear-sighted and blindfolded Justices",
+ "authors": [
+  "J Resnik",
+  "D Curtis"
+ ],
+ "year": 2015,
+ "venue": "J Resnik, D Curtis - Genealogies of Legal Vision, 2015 - books.google.com",
+ "citation_count": 3,
+ "abstract": "… the sighted Justice appears to be corruptible and the blindfolded Justice impervious to the needy before her. In short, the blindfold remained a ready mark of earthly Justice's limitations. …",
+ "url": "https://books.google.com/books?hl=zh-CN&lr=&id=yVnLCQAAQBAJ&oi=fnd&pg=PA201&dq=blindfolded+justice&ots=8P4wgMsWGu&sig=znxykFfkEZK90kQl5dadn1eY3Hs",
+ "pdf_links": [],
+ "themes": [
+  "blindfolded justice"
+ ],
+ "source_query": "blindfolded justice"
+},
+{
+ "title": "Iconologia da justiça: nota histórica",
+ "authors": [
+  "AVC Val",
+  "FB Costa",
+  "H Costa"
+ ],
+ "year": 2008,
+ "venue": "AVC Val, FB Costa, H Costa - … Horizonte: Tribunal de Justiça de …, 2008 - bd-login.tjmg.jus.br",
+ "citation_count": 0,
+ "abstract": "… dos estudos de iconografia e iconologia2, … da Justiça. Partindo da análise histórica das imagens, busca-se identificar quais foram as representações simbólicas atribuídas à Justiça ao …",
+ "url": "https://bd-login.tjmg.jus.br/jspui/bitstream/tjmg/547/1/NHv1852008.pdf",
+ "pdf_links": [
+  "https://bd-login.tjmg.jus.br/jspui/bitstream/tjmg/547/1/NHv1852008.pdf"
+ ],
+ "themes": [
+  "Justiça de olhos vendados",
+  "iconografia da Justiça"
+ ],
+ "source_query": "iconografia da Justiça | Justiça de olhos vendados"
+},
+{
+ "title": "A simbologia da justiça restaurativa",
+ "authors": [
+  "BA Lacerda"
+ ],
+ "year": 2025,
+ "venue": "BA Lacerda - Revista de Doutrina Jurídica, 2025 - revistajuridica.tjdft.jus.br",
+ "citation_count": 0,
+ "abstract": "… Nesse sentido, a pintura de Giotto transmite uma ideia de justiça mais próxima das pretensões da justiça restaurativa do que as atuais justiças vendadas, pois é “uma justiça relacional …",
+ "url": "https://revistajuridica.tjdft.jus.br/rdj/article/view/1086",
+ "pdf_links": [
+  "https://revistajuridica.tjdft.jus.br/rdj/article/download/1086/226"
+ ],
+ "themes": [
+  "Justiça de olhos vendados",
+  "iconografia da Justiça"
+ ],
+ "source_query": "iconografia da Justiça | Justiça de olhos vendados"
+},
+{
+ "title": "A auréola do imperador: ensaio sobre arte e direito na iconografia política medieval",
+ "authors": [
+  "RL Simioni"
+ ],
+ "year": 2021,
+ "venue": "RL Simioni - 2021 - books.google.com",
+ "citation_count": 5,
+ "abstract": "… personificação da imagem viva da Justiça. O príncipe não é mais a imagem viva da Graça de Deus, mas a imagem viva da Justiça, a personificação humana da Justiça, que era uma …",
+ "url": "https://books.google.com/books?hl=zh-CN&lr=&id=dCE_EAAAQBAJ&oi=fnd&pg=PA127&dq=iconografia+da+Justi%C3%A7a&ots=et7nXgRucI&sig=nfM8vtSdi6SwNixWa_hPqeHwC_0",
+ "pdf_links": [],
+ "themes": [
+  "iconografia da Justiça"
+ ],
+ "source_query": "iconografia da Justiça"
+},
+{
+ "title": "As representações da Justiça em Gil Vicente ea relação do dramaturgo com a arte manuelina",
+ "authors": [
+  "LU Afonso"
+ ],
+ "year": 2009,
+ "venue": "LU Afonso - Teatro do mundo: Teatro e justiça. Afinidades electivas, 2009 - ler.letras.up.pt",
+ "citation_count": 2,
+ "abstract": "… , dá lugar a um desdobramento da iconografia da Justiça, clarificando-se a diferenciação … pela diferenciação entre a Justiça e a Injustiça.A grande influência da iconografia dos Vícios e …",
+ "url": "https://ler.letras.up.pt/uploads/ficheiros/10231.pdf",
+ "pdf_links": [
+  "https://ler.letras.up.pt/uploads/ficheiros/10231.pdf"
+ ],
+ "themes": [
+  "iconografia da Justiça"
+ ],
+ "source_query": "iconografia da Justiça"
+},
+{
+ "title": "Alegorias da justiça ea república brasileira (exercício de leitura iconológica)",
+ "authors": [
+  "MR Cabeceiras",
+  "H Alem…"
+ ],
+ "year": 2019,
+ "venue": "MR Cabeceiras, H Alem… - … e Diálogos: Revista …, 2019 - revistas.uneb.br",
+ "citation_count": 0,
+ "abstract": "… uma Justiça pouco afeita a ares de soberana anterior aos ventos revolucionários de Paris (Anexo F) que não parece ter relação com a Justiça … São ainda característicos da \"Justiça\" de …",
+ "url": "https://www.revistas.uneb.br/nhipe/article/view/9450",
+ "pdf_links": [
+  "https://www.revistas.uneb.br/index.php/nhipe/article/download/9450/6299"
+ ],
+ "themes": [
+  "alegorias femininas da justiça",
+  "iconografia da Justiça"
+ ],
+ "source_query": "iconografia da Justiça | alegorias femininas da justiça"
+},
+{
+ "title": "José de Seabra da Silva ea sua família: iconografia e mobilidade social no antigo regime",
+ "authors": [
+  "M Gorjão‑Henriques"
+ ],
+ "year": 2013,
+ "venue": "M Gorjão‑Henriques - Direito e Justiça, 2013 - revistas.ucp.pt",
+ "citation_count": 3,
+ "abstract": "Objecto deste trabalho é a figura de José de Seabra da Silva, perso‑nagem maior e algo controversa da política portuguesa nos reinados de Dom José I e de Dona Maria I. O leitor …",
+ "url": "https://revistas.ucp.pt/index.php/direitoejustica/article/view/9896/9620",
+ "pdf_links": [
+  "https://revistas.ucp.pt/index.php/direitoejustica/article/view/9896/9620"
+ ],
+ "themes": [
+  "iconografia da Justiça"
+ ],
+ "source_query": "iconografia da Justiça"
+},
+{
+ "title": "As imagens do Juízo Final: Julgamento, poder e soberania a partir da iconografia de Giotto e Michelangelo",
+ "authors": [
+  "TGD Santos",
+  "KMM Moreira"
+ ],
+ "year": 2022,
+ "venue": "TGD Santos, KMM Moreira - RJLB–Revista Jurídica Luso-Brasileira, 2022 - cidp.pt",
+ "citation_count": 0,
+ "abstract": "Resumo: O artigo propõe o aprofundamento dos conceitos de “julgamento” e “justiça” na … sentido das simbologias do “julgamento” e “justiça”, decorrendo de uma análise pregressa de …",
+ "url": "https://www.cidp.pt/revistas/rjlb/2022/1/2022_01_1561_1593.pdf",
+ "pdf_links": [
+  "https://www.cidp.pt/revistas/rjlb/2022/1/2022_01_1561_1593.pdf"
+ ],
+ "themes": [
+  "iconografia da Justiça"
+ ],
+ "source_query": "iconografia da Justiça"
+},
+{
+ "title": "Para além da religião? Justiça de Deus, justiça do homem e as representações visuais do Juízo Final",
+ "authors": [
+  "T Quírico"
+ ],
+ "year": 2016,
+ "venue": "T Quírico - Antíteses, 2016 - redalyc.org",
+ "citation_count": 1,
+ "abstract": "… e não em um tribunal, palácio de justiça ou algum outro edifício que se relacionaria de forma mais evidente a julgamentos ou à justiça terrena, a singular iconografia da cena permite a …",
+ "url": "https://www.redalyc.org/pdf/1933/193346401006.pdf",
+ "pdf_links": [
+  "https://www.redalyc.org/pdf/1933/193346401006.pdf"
+ ],
+ "themes": [
+  "iconografia da Justiça"
+ ],
+ "source_query": "iconografia da Justiça"
+},
+{
+ "title": "A JUSTIÇA NOS SÉCULOS XIII E XIV SEGUNDO GIOTTO DI BONDONE E TOMÁS DE AQUINO: IMAGENS E TEXTOS COMO FONTE.",
+ "authors": [
+  "MA Lóde-Nunes",
+  "T Oliveira"
+ ],
+ "year": 2023,
+ "venue": "MA Lóde-Nunes, T Oliveira - Brathair, 2023 - openurl.ebsco.com",
+ "citation_count": 0,
+ "abstract": "… justiça divina e laica, questão que instiga o desenvolvimento deste estudo que tem como objetivo compreender o conceito de justiça … que, também faz parte da iconografia da justiça, é …",
+ "url": "https://openurl.ebsco.com/contentitem/gcd:182262254?sid=ebsco:plink:crawler-gcd&id=ebsco:gcd:182262254&crl=c&jrnl=15199053",
+ "pdf_links": [],
+ "themes": [
+  "iconografia da Justiça"
+ ],
+ "source_query": "iconografia da Justiça"
+},
+{
+ "title": "Nossa Senhora das Mercês: um caso de interesse para a Justiça",
+ "authors": [
+  "B Coelho",
+  "H David",
+  "MRE Quites"
+ ],
+ "year": 2003,
+ "venue": "B Coelho, H David, MRE Quites - Imagem Brasileira, 2003 - eba.ufmg.br",
+ "citation_count": 1,
+ "abstract": "… : a capa complementa a iconografia de Nossa Senhora das Mercês, embora seja colorida e não branca (certamente a devota não tinha conhecimento da iconografia) , e a imagem …",
+ "url": "https://www.eba.ufmg.br/revistaceib/index.php/imagembrasileira/article/view/46",
+ "pdf_links": [
+  "https://www.eba.ufmg.br/revistaceib/index.php/imagembrasileira/article/download/46/34"
+ ],
+ "themes": [
+  "iconografia da Justiça"
+ ],
+ "source_query": "iconografia da Justiça"
+}
+]);
